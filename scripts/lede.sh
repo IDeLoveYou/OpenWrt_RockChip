a63bcf26bebab697892a9c19e5c875ecd23d16ab
IDeLoveYou <2584787445@qq.com>
2024-10-19 23:51:38 +0800

    Update lede.sh

--- scripts/lede.sh
+++ scripts/lede.sh
@@ -19,7 +19,7 @@ rm -rf openwrt-package/verysync
 rm -rf openwrt-package/luci-app-verysync
 
 # apppppppp
-git clone --depth=1 https://github.com/DHDAXCW/dhdaxcw-app
+git clone --depth=1 https://github.com/IDeLoveYou/dhdaxcw-app
 
 # Add luci-app-ssr-plus
 git clone --depth=1 https://github.com/fw876/helloworld
